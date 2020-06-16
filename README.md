a76fed1ad3d39526f21c68245993a11203ec645a
unknown <bourantas@ceid.upatras.gr>
2020-06-16 13:13:51 +0300

    google calendar integration

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # openweatherWidget
-A desktop widget displaying data from openweather API using Electron.
+A desktop widget displaying weather data from openweather API and events from Google Calendar API using Electron.js .
 
 
 <img src="screenshot.PNG" width="400" height="210">
--- main.js
+++ main.js
@@ -3,25 +3,31 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 const axios = require("axios");
-const { app, BrowserWindow } = electron;
-
-require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+const { app, BrowserWindow, ipcMain } = electron;
+const fs = require("fs");
+const { google } = require("googleapis");
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = "token.json";
 
 let mainWindow;
 let city = "Patras";
 
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function createMainWindow() {
   mainWindow = new BrowserWindow({
-    vibrancy: "ultra-dark",
     frame: false,
-    width: 400,
-    height: 210,
-    x: 1500,
-    y: 70,
-    resizable: false,
+    width: 350,
+    height: 470,
+    x: 1560,
+    y: 0,
+    resizable: true,
     fullscreenable: false,
-    transparent: true,
     opacity: 0.8,
     skipTaskbar: true,
     webPreferences: {
@@ -53,9 +59,9 @@ async function fetchWeather() {
     )
     .catch((err) => console.log(err));
 
-  mainWindow.webContents.send("weather-data", response.data);
-
   console.log(response.data);
+
+  mainWindow.webContents.send("weather-data", response.data);
 }
 
 function fetchDatetime() {
@@ -90,6 +96,12 @@ app.on("ready", function () {
   mainWindow.webContents.on("did-finish-load", function () {
     fetchWeather();
     fetchDatetime();
+    // Load client secrets from a local file.
+    fs.readFile("credentials.json", (err, content) => {
+      if (err) return console.log("Error loading client secret file:", err);
+      // Authorize a client with credentials, then call the Google Calendar API.
+      authorize(JSON.parse(content), listEvents);
+    });
   });
 });
 
@@ -106,4 +118,120 @@ app.on("activate", function () {
   if (mainWindow === null) createMainWindow();
 });
 
+ipcMain.on("get-events", (event) => {
+  // Load client secrets from a local file.
+  fs.readFile("credentials.json", (err, content) => {
+    if (err) return console.log("Error loading client secret file:", err);
+    // Authorize a client with credentials, then call the Google Calendar API.
+    authorize(JSON.parse(content), listEvents);
+  });
+});
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Google Calendar API stuff
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  code = process.env.GOOGLE_CAL_KEY;
+
+  if (!code) {
+    mainWindow.webContents.send("events-mess", {
+      mess: "Authorize this app by visiting this url: " + authUrl,
+    });
+    return;
+  }
+
+  oAuth2Client.getToken(code, (err, token) => {
+    if (err) return console.error("Error retrieving access token", err);
+    oAuth2Client.setCredentials(token);
+    // Store the token to disk for later program executions
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+      if (err) return console.error(err);
+      console.log("Token stored to", TOKEN_PATH);
+    });
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listEvents(auth) {
+  const calendar = google.calendar({ version: "v3", auth });
+  calendar.events.list(
+    {
+      calendarId: "primary",
+      timeMin: new Date().toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: "startTime",
+    },
+    (err, res) => {
+      if (err) return console.log("The API returned an error: " + err);
+      const events = res.data.items;
+      console.log(res.data);
+      if (events.length) {
+        let eventsData = [];
+
+        console.log("Upcoming 10 events:");
+        events.map((event, i) => {
+          const start = event.start.dateTime || event.start.date;
+
+          let options = {
+              weekday: "long",
+              timeZone: `Europe/Athens`,
+              year: "numeric",
+              month: "numeric",
+              day: "numeric",
+              hour: "numeric",
+              minute: "numeric",
+            },
+            formatter = new Intl.DateTimeFormat([], options);
+
+          let datetime = formatter.format(new Date(start));
+
+          eventsData.push({ datetime: datetime, descr: event.summary });
+        });
+
+        console.log(eventsData);
+        mainWindow.webContents.send("events-data", eventsData);
+      } else {
+        console.log("No upcoming events found.");
+        mainWindow.webContents.send("events-data", []);
+      }
+    }
+  );
+}
